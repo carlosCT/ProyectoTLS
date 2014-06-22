3f5c1b268cd01ecfd2057532c2915a777fe297f0
Alejandro <Alejandro@192.168.1.39>
2014-06-22 02:15:59 -0500

    generacion de patrones

---
+++ ProyectoTLS/src/inf/pucp/edu/pe/CargaDatos/LeeArchivo.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inf.pucp.edu.pe.CargaDatos;
+
+import inf.pucp.edu.pe.cliente.ClienteVehiculos;
+import inf.pucp.edu.pe.modelo.Coordernada;
+import inf.pucp.edu.pe.modelo.Ruta;
+import inf.pucp.edu.pe.modelo.Vehiculo;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ *
+ * @author Alejandro
+ */
+
+
+public class LeeArchivo {
+
+     public ArrayList<Vehiculo> autos = new ArrayList<>();
+    public  String s;//"/Users/Alejandro/Documents/PUCP/2014-1/dp1/Archivos2";
+    public String archact;
+    public boolean ter= false;
+        
+       
+    
+public LeeArchivo(String s){
+       this.s=s;
+};    
+
+ public  void leerFile(File f) throws IOException {
+
+        String id = null;
+        ArrayList<Ruta> rutas = new ArrayList<Ruta>();
+
+        if (f.getName().length() > 11) {
+            //String name =f.getName();
+            archact=f.getName().substring(4, 10);
+            
+            // System.out.println(archact);
+            id = f.getName().substring(4, 10);
+            FileReader entrada = null;
+            try {
+                entrada = new FileReader(f);
+            } catch (FileNotFoundException ex) {
+            }
+
+            try {
+                BufferedReader in;
+                in = new BufferedReader(entrada);
+                String data ;
+                data = in.readLine();
+                int tiempo, x, y;
+                String hora, min, seg;
+
+                while (data != null) {
+                    hora = data.substring(0, 2);
+                    min = data.substring(3, 5);
+                    seg = data.substring(6, 8);
+
+                    tiempo = Integer.parseInt(hora) * 3600 + Integer.parseInt(min) * 60 + Integer.parseInt(seg);
+                    data = data.substring(9);
+                    ArrayList<Coordernada> listcor = new ArrayList<Coordernada>();
+                    while (data.length() > 0) {
+                        int pos = data.indexOf(",");
+                        x = Integer.parseInt(data.substring(0, pos));
+                        data = data.substring(pos + 1);
+
+                        pos = data.indexOf(",");
+                        if (pos == -1) {
+                            pos = data.indexOf("*");
+                            y = Integer.parseInt(data.substring(0, pos));
+
+                        }
+                        y = Integer.parseInt(data.substring(0, pos));
+                        data = data.substring(pos + 1);
+                        Coordernada cor = new Coordernada(x, y);
+                        listcor.add(cor);
+//        System.out.print(x);
+//        System.out.print(",");
+//        System.out.print(y);
+//        System.out.print("\n");
+
+                    }
+
+//System.out.println(data); 
+                    Ruta ruta = new Ruta(tiempo, listcor);
+                    rutas.add(ruta);
+
+                    data = in.readLine();
+                }
+
+            } catch (IOException e) {
+                System.out.println("no se encontro!");
+            }
+             try {
+            entrada.close();
+        } catch (IOException ex) {}
+        }
+        
+        Vehiculo vehiculo = new Vehiculo(id, rutas);
+        autos.add(vehiculo);
+    }
+
+ 
+ public  void imprimir(){
+     for(int i=0;i<autos.size();i++){
+         System.out.println(autos.get(i).id);
+         for(int j=0;j<autos.get(i).rutas.size();j++){
+             System.out.println(autos.get(i).rutas.get(j).horaSalida);
+            System.out.print(" ");
+             for(int k=0;k<autos.get(i).rutas.get(j).ruta.size();k++){
+              System.out.print(autos.get(i).rutas.get(j).ruta.get(k).x);  
+              System.out.print(",");
+              System.out.print(autos.get(i).rutas.get(j).ruta.get(k).y); 
+              System.out.print("\n");
+             }
+         }
+         
+     }
+ }
+ 
+    public void Cargar() throws IOException  {
+        File directorio = new File(s);
+        String[] listaDirectorio = directorio.list();
+        
+  if (listaDirectorio == null) {
+            System.out.println("No hay ficheros en el directorio especificado");
+        } else {
+            
+            for (int x = 1; x <listaDirectorio.length; x++) //System.out.println(listaDirectorio[x]);                      
+            {
+                try {
+                    leerFile(new File(s + "/" + listaDirectorio[x]));
+                } catch (IOException ex) {
+                    Logger.getLogger(LeeArchivo.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        
+        ter=true;
+
+       //ClienteVehiculos.cargarVehiculos(autos);
+      
+       // ClienteVehiculos.actualizarVehiculos();
+  //imprimir();
+        
+    }
+    
+
+
+     
+    }
+
+    /**
+     * @return the s
+     */
+  
+    
+}
--- ProyectoTLS/src/inf/pucp/edu/pe/CargaDatos/MenuPrincipalCarga.java
+++ ProyectoTLS/src/inf/pucp/edu/pe/CargaDatos/MenuPrincipalCarga.java
@@ -9,7 +9,10 @@ package inf.pucp.edu.pe.CargaDatos;
 import inf.pucp.edu.pe.vista.principal.MenuPrincipal;
 import java.awt.Color;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.UIManager;
 
@@ -20,6 +23,10 @@ import javax.swing.UIManager;
 
 
 public class MenuPrincipalCarga extends javax.swing.JFrame {
+    private String file;
+    public String car;
+
+LeeArchivo arch = null;
 
     /**
      * Creates new form MenuPrincipalCarga
@@ -41,10 +48,9 @@ public class MenuPrincipalCarga extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        NombreArch = new javax.swing.JTextField();
         jProgressBar1 = new javax.swing.JProgressBar();
         jButton2 = new javax.swing.JButton();
-        jLabel3 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jRadioButton1 = new javax.swing.JRadioButton();
@@ -52,6 +58,7 @@ public class MenuPrincipalCarga extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
         jButton5 = new javax.swing.JButton();
+        nombre = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -70,10 +77,18 @@ public class MenuPrincipalCarga extends javax.swing.JFrame {
             }
         });
 
+        NombreArch.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                NombreArchActionPerformed(evt);
+            }
+        });
+
         jProgressBar1.setBackground(new java.awt.Color(255, 255, 255));
         jProgressBar1.setForeground(new java.awt.Color(0, 153, 204));
         jProgressBar1.setToolTipText("");
         jProgressBar1.setBorderPainted(false);
+        jProgressBar1.setFocusCycleRoot(true);
+        jProgressBar1.setFocusTraversalKeysEnabled(false);
         jProgressBar1.setStringPainted(true);
         jProgressBar1.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
@@ -87,9 +102,6 @@ public class MenuPrincipalCarga extends javax.swing.JFrame {
                 btncerrar(evt);
             }
         });
-
-        jLabel3.setFont(new java.awt.Font("Lucida Grande", 0, 10)); // NOI18N
-        jLabel3.setText("tiempo restante: 10:56:04");
 
         jButton3.setText("Iniciar");
         jButton3.setToolTipText("");
@@ -144,6 +156,7 @@ public class MenuPrincipalCarga extends javax.swing.JFrame {
                 .addGap(14, 14, 14))
         );
 
+        nombre.setText("Nombre Archivo");
         setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -158,54 +171,58 @@ public class MenuPrincipalCarga extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(42, 42, 42)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 518, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addGroup(layout.createSequentialGroup()
                                         .addComponent(jLabel1)
                                         .addGap(18, 18, 18)
                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                             .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 297, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(120, 120, 120)
-                                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(130, 130, 130)
-                                .addComponent(jButton4))
-                            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                                            .addComponent(NombreArch, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGap(17, 17, 17)
+                                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(120, 120, 120)
+                                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(130, 130, 130)
+                                        .addComponent(jButton4)))
+                                .addGap(142, 142, 142))))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(166, 166, 166)
                         .addComponent(jRadioButton1)
                         .addGap(131, 131, 131)
-                        .addComponent(jRadioButton2)))
+                        .addComponent(jRadioButton2))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addContainerGap()
+                            .addComponent(nombre, javax.swing.GroupLayout.PREFERRED_SIZE, 498, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addGap(53, 53, 53)
+                            .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 518, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap(33, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel1)
-                            .addComponent(jButton1))
-                        .addGap(17, 17, 17)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jRadioButton1)
-                            .addComponent(jRadioButton2))
-                        .addGap(59, 59, 59)
-                        .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(29, 29, 29)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(NombreArch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1)
+                    .addComponent(jButton1))
+                .addGap(17, 17, 17)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jRadioButton1)
+                    .addComponent(jRadioButton2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(nombre)
+                .addGap(42, 42, 42)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton3)
                     .addComponent(jButton4))
@@ -221,9 +238,13 @@ public class MenuPrincipalCarga extends javax.swing.JFrame {
       JFileChooser dlg = new  JFileChooser();
       int opt = dlg.showOpenDialog(this);
       if(opt==JFileChooser.APPROVE_OPTION ){
-        String file = dlg.getSelectedFile().getPath();
+         file = dlg.getSelectedFile().getPath();
     }
-          
+         int n = file.lastIndexOf('/');
+         file = file.substring(0, n);
+         System.out.print("---"+ file + "---");
+         arch = new LeeArchivo(file);
+          NombreArch.setText(file);
      
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -237,13 +258,26 @@ public class MenuPrincipalCarga extends javax.swing.JFrame {
        
     }//GEN-LAST:event_jProgressBar1StateChanged
 
+    @SuppressWarnings("empty-statement")
     private void btnIniciarCarga(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIniciarCarga
-       jProgressBar1.setValue(99);
+       
+        jProgressBar1.setIndeterminate(true);
+         this.actualizacion.start();
+         this.cargahilo.start();
+        
+    
+       //
+       
+       
     }//GEN-LAST:event_btnIniciarCarga
 
     private void btnCancelar(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelar
         jProgressBar1.setValue(0);
     }//GEN-LAST:event_btnCancelar
+
+    private void NombreArchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NombreArchActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_NombreArchActionPerformed
 
     /**
      * @param args the command line arguments
@@ -279,8 +313,29 @@ public class MenuPrincipalCarga extends javax.swing.JFrame {
             }
         });
     }
-
+    Thread actualizacion = new Thread(){
+        
+        public void run(){
+            while(!arch.ter)
+            nombre.setText(file + "/veh" + arch.archact + ".rut.txt");
+            
+        }
+    };
+    
+    Thread cargahilo = new Thread(){
+        public void run (){
+            try {
+            
+            arch.Cargar();}catch(IOException e){};
+           
+        //
+        jProgressBar1.setIndeterminate(false);
+        jProgressBar1.setString("100%"); 
+        }
+    };
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField NombreArch;
     public static javax.swing.ButtonGroup TipoCarga;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
@@ -289,19 +344,20 @@ public class MenuPrincipalCarga extends javax.swing.JFrame {
     private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JProgressBar jProgressBar1;
     private javax.swing.JRadioButton jRadioButton1;
     private javax.swing.JRadioButton jRadioButton2;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel nombre;
     // End of variables declaration//GEN-END:variables
 private void inciaCarga()
 {
    
     
 }
+
+
 
 }
